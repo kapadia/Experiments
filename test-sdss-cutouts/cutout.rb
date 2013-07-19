a9887253f33de2557efd289125d92e1a7d0d4b1c
Amit Kapadia <amit@zooniverse.org>
2013-07-19 11:54:26 -0500

    no reprojection

--- test-sdss-cutouts/cutout.rb
+++ test-sdss-cutouts/cutout.rb
@@ -11,37 +11,18 @@ def create_field_url(run, rerun, camcol, field, band)
   return "http://das.sdss.org/imaging/#{run}/#{rerun}/corr/#{camcol}/fpC-#{run_padded}-#{band}#{camcol}-#{field_padded}.fit.gz"
 end
 
-def montage_make_header(ra, dec)
-  filename = File::join(File.dirname(__FILE__), 'header.tmp')
-  
-  # arcsec_per_pixel = 0.396127
-  arcsec_per_pixel = 0.15
-  dimension = 512
-  
-  fov_arcsec = arcsec_per_pixel * dimension
-  fov_degrees = "%0.6f" % (fov_arcsec / 3600.0)
-  
-  mHdr = "mHdr -p #{arcsec_per_pixel} \"#{ra} #{dec}\" #{fov_degrees} #{filename}"
-  `#{mHdr}`
-end
-
 def montage_make_cutout(ra, dec, infile, outfile)
-  montage_make_header(ra, dec)
   
-  `mProjectPP #{infile} #{outfile} header.tmp`
+  s1 = 0.396127 # Natural scale of SDSS DR7
+  s2 = 0.15 # Scale for galaxy zoo quench
+  dimension = 424 # target cutout pixel dimension
   
-  `rm header.tmp`
-  `rm #{outfile}_area.fits`
+  factor = s2 / s1
+  fov_degrees = "%0.11f" % (s2 * dimension / 3600.0)
+  `mSubimage #{infile} #{outfile} #{ra} #{dec} #{fov_degrees}`
+  `mShrink #{outfile} #{outfile} #{factor}`
 end
 
-def montage_subimage(ra, dec, infile, outfile)
-  arcsec_per_pixel = 0.15
-  dimension = 424
-  fov_arcsec = arcsec_per_pixel * dimension
-  fov_degrees = "%0.11f" % (fov_arcsec / 3600.0)
-  
-  `mSubimage #{infile} #{outfile}.fits #{ra} #{dec} #{fov_degrees} #{fov_degrees}`
-end
 
 # Get SDSS object ids
 objids = File.read("data/control.tab").split("\n").collect{ |row| row.split(/\s+/)[0] }
@@ -63,10 +44,10 @@ while objids.count > 0
       filename = url.split('/').last
       
       infile = File::join(File.dirname(__FILE__), 'FITS', filename)
-      outfile = File::join('cutouts', "#{objid}_#{band}")
+      outfile = File::join('cutouts', "#{objid}_#{band}.fits")
       
       `curl -o #{infile} '#{url}'` unless File.exists?(infile)
-      montage_subimage(ra, dec, infile, outfile) unless File.exists?("#{outfile}.fits")
+      montage_make_cutout(ra, dec, infile, outfile) unless File.exists?("#{outfile}")
     end
   end
 
